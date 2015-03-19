7e438d5177770f2cd597b5f1800df2261bbb378b
Daniel Graham <danielgraham@Daniels-MacBook-Air-2.local>
2015-03-19 00:21:29 -0700

    Implemented 'CustomEvent' class with unit tests

--- app/js/utils/customEvent.js
+++ app/js/utils/customEvent.js
@@ -3,14 +3,18 @@
  * @constructor
  */
 var CustomEvent = function(){
-
+    this._callbacks = [];
 };
 
 /**
- * Fire the event
+ * Trigger the event
  */
 CustomEvent.prototype.trigger = function(){
 
+    // Call all of the callbacks that subscribed to this event
+    for(var i=0; i<this._callbacks.length; i++){
+        this._callbacks[i].apply(this, arguments);
+    }
 };
 
 /**
@@ -18,7 +22,7 @@ CustomEvent.prototype.trigger = function(){
  * @param cb {function}
  */
 CustomEvent.prototype.subscribe = function(cb){
-
+    this._callbacks.push(cb);
 };
 
 /**
@@ -26,7 +30,11 @@ CustomEvent.prototype.subscribe = function(cb){
  * @param cb {function} Optional. If null unsubscribes all callbacks
  */
 CustomEvent.prototype.unsubscribe = function(cb){
-
+    for(var i=this._callbacks.length - 1; i>=0; i--){
+        if(typeof(cb)==='undefined' || this._callbacks[i] == cb){
+            this._callbacks.splice(i, 1);
+        }
+    }
 };
 
 module.exports = CustomEvent;
--- test/unit/utils/customEvent.unit.js
+++ test/unit/utils/customEvent.unit.js
@@ -1,6 +1,100 @@
-var assert = require("assert");
+var assert = require("assert"),
+    customEvent = require("../../../app/js/utils/customEvent");
 
-describe('sample', function(){
-    console.log("testing");
-    assert.equal(true, true);
-})
+describe('subscribe', function(){
+    describe('one callback', function(){
+        it("should fire one callback when you trigger an event and have the correct arguments", function(done){
+            var event = new customEvent();
+            event.subscribe(function(){
+                assert.equal(arguments[0], "hello");
+                assert.equal(arguments[1], "world");
+                done();
+            });
+
+            setTimeout(function(){
+                event.trigger("hello", "world");
+            }, 0);
+        });
+    });
+
+    describe('multiple callbacks', function(){
+        it("should fire multiple callbacks when you trigger an event and have the correct arguments", function(done){
+            var event = new customEvent();
+            var callbacks = 0;
+
+            event.subscribe(function(){
+                assert.equal(arguments[0], "hello");
+                assert.equal(arguments[1], "world");
+                if(callbacks++ == 2)
+                    done();
+            });
+
+            event.subscribe(function(){
+                assert.equal(arguments[0], "hello");
+                assert.equal(arguments[1], "world");
+                if(callbacks++ == 2)
+                    done();
+            });
+
+            event.subscribe(function(){
+                assert.equal(arguments[0], "hello");
+                assert.equal(arguments[1], "world");
+                if(callbacks++ == 2)
+                    done();
+            });
+
+            setTimeout(function(){
+                event.trigger("hello", "world");
+            }, 0);
+        });
+    });
+});
+
+describe('unsubscribe', function(){
+
+    describe('subscribe and then unsubscribe', function(){
+        it('should not fire the callback', function(){
+            var event = new customEvent();
+            var cb = function(){
+                assert.equal(true, false, "Should not reach this function, should be unsubscribed");
+            };
+            event.subscribe(cb);
+            event.unsubscribe(cb);
+            event.trigger();
+        });
+    });
+
+    describe('subscribe multiple callbacks and then unsubscribe all of them', function(){
+        it('should not fire any of the callbacks', function(){
+            var event = new customEvent();
+            var cb1 = function(){
+                assert.equal(true, false, "Should not reach this function, should be unsubscribed");
+            };
+            var cb2 = function(){
+                assert.equal(true, false, "Should not reach this function, should be unsubscribed");
+            };
+            event.subscribe(cb1);
+            event.subscribe(cb2);
+            event.unsubscribe();
+            event.trigger();
+        });
+    });
+
+    describe('subscribe multiple callbacks and then unsubscribe all of them', function(){
+        it('should fire only one callback', function(done){
+            var event = new customEvent();
+            var cb1 = function(){
+                assert.equal(true, false, "Should not reach this function, should be unsubscribed");
+            };
+            var cb2 = function(){
+                assert.equal(arguments[0], "foobar", "This should be reached");
+                done();
+            };
+            event.subscribe(cb1);
+            event.subscribe(cb2);
+            event.unsubscribe(cb1);
+            event.trigger("foobar");
+        });
+    });
+
+});
