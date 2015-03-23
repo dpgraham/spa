67c46684fa43f5881b63610f9a9e7d1081cb717a
Daniel Graham <danielgraham@Daniels-MacBook-Air-2.local>
2015-03-22 20:14:01 -0700

    Added a tag to the app.html which shows an error message.

--- app/js/views/appView.js
+++ app/js/views/appView.js
@@ -1,12 +1,17 @@
 var baseView = require("./baseView");
 var template = require("../templates/app.html");
 var categoriesView = require("./categoriesView");
+var BaseModel = require("../models/baseModel");
 
 var AppView = function(el){
     this.template = template;
     this.el = el;
     this.render();
     var sidebarEl = document.getElementById("sidebar");
+
+    BaseModel.onError.subscribe(function(){
+        document.getElementById("error_message").className = "alert alert-danger";
+    });
 
     this.categoriesView = new categoriesView(sidebarEl);
 };
