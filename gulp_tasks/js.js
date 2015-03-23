118802710378b6fe60a7b924112ef931c1c2c12d
Daniel Graham <danielgraham@Daniels-MacBook-Air-2.local>
2015-03-22 19:37:08 -0700

    Added Javascript compression

--- gulp_tasks/js.js
+++ gulp_tasks/js.js
@@ -1,22 +1,20 @@
 var browserify = require("browserify");
 var gulp = require("gulp");
 var gulpUtil = require("gulp-util");
-var uglifyify = require("uglifyify");
 var hbsfy = require("hbsfy").configure({
     extensions: ["html"]
 });;
 var source = require('vinyl-source-stream');
+var minifyify = require('minifyify');
 
 gulp.task("js", function(){
     var out = browserify('./app/js/main.js', {debug: !gulpUtil.env.minify});
 
-    out.transform(hbsfy);
-
-    // If the minify flag is set, then uglify the Javascript
     if(gulpUtil.env.minify){
-        out.transform(uglifyify);
+        out.plugin("minifyify", {map: false});
     }
 
+    out.transform(hbsfy);
 
     // Bundle it and push it to the ./dist folder
     out.bundle()
